5c50435fbb84006697ee79e1815998b69a123063
Galdrial <camerano.simo@gmail.com>
2025-12-15 05:54:50 +0100

    fix: uncomment StrictMode in main.tsx for improved performance

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-/* import { StrictMode } from 'react' */
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
 import App from './App.tsx';
@@ -6,9 +6,9 @@ import './index.css';
 import { store } from './redux/store.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  /* <StrictMode> */
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  /* </StrictMode> */
+  </StrictMode>
 )
